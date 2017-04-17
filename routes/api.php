bf5a3898b8586d38fb4645d46762c172f27a67eb
patawat <patawat.wtk@gmail.com>
2017-04-17 14:07:59 +0700

    Fix api route

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,12 @@ $routeResource = [
         'edit'
     ]
 ];
-
-<<<<<<< HEAD
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-
 Route::get('scene', 'SceneController@index');
-=======
 Route::group(['middleware' => 'auth:api'], function() {
     Route::resource('scene', 'SceneController');
     // Route::get('scene', 'SceneController@index');
 });
-<<<<<<< HEAD
-
 Route::post('register', 'UserController@store');
-=======
->>>>>>> dec26b35100a57578d38a2f86d28a1758a1d286f
->>>>>>> 0882a45add6a4127e43863be9f21a49e6bce6a16
