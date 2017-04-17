4382dc006db437bf1f38092c331dd8e14be0aedf
root <root@ip-172-31-5-54.ap-southeast-1.compute.internal>
2017-04-17 06:43:26 +0000

    fix merge2

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test101',
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        //
+    }
+}
