11f341f60d1e9b1a7d6964d58570adc88eae940a
patawat <patawat.wtk@gmail.com>
2017-04-24 15:23:50 +0700

    Update migration

--- database/migrations/2017_04_20_195456_create_stories_table.php
+++ database/migrations/2017_04_20_195456_create_stories_table.php
@@ -30,6 +30,8 @@ class CreateStoriesTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('stories');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
